a762a4b4e4db3649f48b560458dd6653c89ac437
San-G1 <sanjeesanji102@gmail.com>
2023-04-29 15:37:24 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('welcome'),
+      centerTitle: true,
+    ),
+  ),
+));
+
